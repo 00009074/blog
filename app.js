253d405cc674b8d204cc15a5a4a52e43e99052d8
00009074 <00009074id@gmail.com>
2021-03-17 21:18:12 +0500

    add views folder

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'pug')
+
+app.use('/static', express.static('public'))
+
 app.get('/', (req, res) => {
-    res.render('Hello!')
+	res.render('home')
+}) 
+
+app.get('/create_note', (req, res) => {
+	res.render('create_note')
+}) 
+
+app.get('/notes', (req, res)=> {
+	//const notes = ['first', 'second', 'third']
+	const notes = require('./db')
+	res.render('all_notes', { notes: notes })
+})
+//mocking the router handling for note detail page
+app.get('/notes/detail', (req, res) => {
+	res.render('note_detail')
 })
 
 app.listen(8000, err => {
-    if (err) throw err
-    console.log("App is running on 8000 port")
+	if (err) throw err
+	console.log("App is running...")
 })
