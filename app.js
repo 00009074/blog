bbdbf95de12386cf11bd319be378a2a9c8347c2e
00009074 <00009074id@gmail.com>
2021-03-18 10:45:21 +0500

    add services

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
-const express = require('express')
-const app = express()
+const path = require("path");
+const fs = require('fs');
+
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+
+const blogs = require("./routes/blogs");
+const posts = require("./routes/posts");
+const getCollection = require("./utils").getCollection;
 
 app.set('view engine', 'pug')
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/static', express.static('public'))
+app.use(bodyParser.json());
+
+app.use("/blogs", blogs);
+app.use("/posts", posts);
 
 app.get('/', (req, res) => {
 	res.render('home')
 }) 
+
+app.get("/archive", (req, res) => {
+	fs.readFile(getCollection('blogs.json'), (err, data) => {
+		if (err) res.sendStatus(500)
+		
+		const posts = JSON.parse(data).filter(post => post.archive == true)
+		res.render("all_posts", { title: "Hey", posts: posts });
+	  })
+	});
 
 app.get('/create_posts', (req, res) => {
 	res.render('create_posts')
 }) 
 
 app.get('/posts', (req, res)=> {
-	//const notes = ['first', 'second', 'third']
-	const posts = require('./db')
+	const posts = ['first', 'second', 'third']
+	//const posts = require('')
 	res.render('all_posts', { posts: posts })
 })
 //mocking the router handling for note detail page
@@ -25,5 +46,5 @@ app.get('/posts/detail', (req, res) => {
 
 app.listen(8000, err => {
 	if (err) throw err
-	console.log("App is running...")
+	console.log("App is running on port 8000...")
 })
---
+++ routes/blogs.js
@@ -0,0 +1,71 @@
+const fs = require('fs')
+const path = require('path')
+
+const express = require("express")
+const router = express.Router()
+
+const Validator = require("../services/validators")
+const DbContext = require("../services/db")
+const root = require("../utils").root;
+const getCollection = require("../utils").getCollection;
+
+const dbc = new DbContext()
+const v = new Validator()
+dbc.useCollection("blogs.json")
+
+router.get("/", (req, res) => {
+  dbc.getAll(
+    records => res.render("all_posts", { posts: records }),
+    () => res.render("all_posts", { posts: null })
+  )
+})
+
+router.get("/create_posts", (req, res) => {
+  res.render("create_posts", {})
+});
+
+router.post("/create_posts", (req, res) => {
+  if (v.isValid(req.body)) {
+    dbc.saveOne(req.body, () => res.render("create_posts", { success: true }))
+  } else {
+    res.render("create_posts", { error: true, success: false })
+  }
+})
+
+router.get('/:id/delete', (req, res) => {
+  dbc.deleteOne(
+    req.params.id, 
+    () => res.redirect('/')),
+    () => res.sendStatus(500)
+})
+
+router.get("/:id/archive", (req, res) => {
+  fs.readFile(getCollection('blogs.json'), (err, data) => {
+    if (err) res.sendStatus(500)
+
+    const posts = JSON.parse(data)
+    const post = posts.filter(post => post.id == req.params.id)[0]
+    const postIdx = posts.indexOf(post)
+    const splicedPost = posts.splice(postIdx, 1)[0]
+    splicedPost.archive = true
+    posts.push(splicedNote)
+
+    fs.writeFile(getCollection('blogs.json'), JSON.stringify(posts), err => {
+      if (err) res.sendStatus(500)
+
+      res.redirect('/posts')
+    })
+    
+  })
+})
+
+router.get("/:id", (req, res) => {
+  dbc.getOne(
+    req.params.id,
+    record => res.render("post_detail", { post: record }),
+    () => res.sendStatus(404)
+  )
+})
+
+module.exports = router;
+
